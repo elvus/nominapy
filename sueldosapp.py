5b5bdc4cbdbde1d5db34eb172b07e37b218110e2
elvus <e.s95@hotmail.com>
2021-03-24 21:26:27 -0300

    Se agregaron los parametros page y rows para limitar los registros mostrados

--- sueldosapp.py
+++ sueldosapp.py
@@ -1,7 +1,6 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 from flask_cors import CORS
 from bson.json_util import dumps
-from requests.api import request
 from sueldos import connection
 import urllib3
 
@@ -12,10 +11,12 @@ cors = CORS(app, resources={r"/api/*": {"origins":"*"}})
 def index():
     return app.send_static_file("index.html")
 
-@app.route("/api/nomina/<path:q>",methods=['GET', 'POST'])
-def api_root(q):
+@app.route("/api/nomina",methods=['GET', 'POST'])
+def api_root():
     db=connection()
-    response=dumps(db.nomina.find({'anio':int(q)}).limit(5))
+    pageNumber = int(request.args.get('page'))
+    nPerPage = int(request.args.get('rows'))
+    response=dumps(db.nomina.find().skip(((pageNumber - 1 ) * nPerPage ) if (pageNumber > 0) else 0).limit(nPerPage))
     return Response(response=response, status=200, mimetype='application/json')
 
 @app.route("/api/<path:q>",methods=['GET', 'POST'])
@@ -23,7 +24,7 @@ def query(q):
     db=connection()
     response=dumps(db.nomina.find({
         "$or":[
-                {"descripcionEntidad": {'$regex': str(urllib3.util.parse_url(q)), "$options" :'i'}}, 
+                {"descripcionEntidad": {'$regex': str(urllib3.util.parse_url(q)), "$( ( pageNumber - 1 ) * nPerPage ) : 0options" :'i'}}, 
                 {"codigoPersona": {'$regex': q}}, 
                 {"nombres": {'$regex': str(urllib3.util.parse_url(q)), "$options" :'i'}},
                 {"apellidos": {'$regex': str(urllib3.util.parse_url(q)), "$options" :'i'}}
